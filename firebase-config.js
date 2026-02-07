560c78fd19ef340a36782ceadb9916b96b3415e2
Ivan Paredes <ivanparedes@Mac-mini-de-Ivan.local>
2026-02-07 15:30:58 -0300

    Ajusta apiKey do Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCZ8PF0tNDtH22TJ0hmDgpthq9DkGc4vfk",
+  apiKey: "AIzaSyBve3yIYedKbE67awrTnzx9AL8os460ppQ",
   authDomain: "attendanceease-xszz9.firebaseapp.com",
   projectId: "attendanceease-xszz9",
   appId: "1:559492052905:web:a773c201dd114ef07576b8",
