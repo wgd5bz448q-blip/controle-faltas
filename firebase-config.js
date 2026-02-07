6635e1fad8218f1676727c4b9728da7672c2d21a
Ivan Paredes <ivanparedes@Mac-mini-de-Ivan.local>
2026-02-07 14:53:00 -0300

    Atualiza apiKey

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBve3yIYedKbE67awrTnzx9AL8os460ppQ",
+  apiKey: "AIzaSyCZ8PF0tNDtH22TJ0hmDgpthq9DkGc4vfk",
   authDomain: "attendanceease-xszz9.firebaseapp.com",
   projectId: "attendanceease-xszz9",
   appId: "1:559492052905:web:a773c201dd114ef07576b8",
