bbd15066ebb6419f086b3af1a304d043b26ca299
Ivan Paredes <ivanparedes@Mac-mini-de-Ivan.local>
2026-02-07 09:02:12 -0300

    Corrige firebase-config

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBve3yIYedKbE67awITnzx9AL8os460ppQ",
+  apiKey: "AIzaSyBve3yIYedKbE67awrTnzx9AL8os460ppQ",
   authDomain: "attendanceease-xszz9.firebaseapp.com",
   projectId: "attendanceease-xszz9",
   appId: "1:559492052905:web:a773c201dd114ef07576b8",
