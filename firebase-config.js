71d5e7d53104f2801ebbde3c94ca635412dcb7f3
Ivan Paredes <ivanparedes@Mac-mini-de-Ivan.local>
2026-02-07 08:19:32 -0300

    Ativa Firebase Firestore no controle de faltas

---
+++ firebase-config.js
@@ -0,0 +1,6 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBve3yIYedKbE67awITnzx9AL8os460ppQ",
+  authDomain: "attendanceease-xszz9.firebaseapp.com",
+  projectId: "attendanceease-xszz9",
+  appId: "1:559492052905:web:a773c201dd114ef07576b8",
+};
